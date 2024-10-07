c06f4942678f5d092098a231a420b516d6d226e2
Kartik Navale <kartik.work019@gmail.com>
2024-10-07 09:18:08 -0700

    Updated rule engine component

--- src/components/RuleEngine.jsx
+++ src/components/RuleEngine.jsx
@@ -3,6 +3,7 @@ import "../components/style.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 const RuleEngine = () => {
   return (
@@ -17,8 +18,9 @@ const RuleEngine = () => {
           <h5 class="mb-3">Rule List</h5>
           <div className="d-flex justify-content-between loyalty-header">
             <div>
+            <Link to='/ViewRuleEngine'> 
               <button
-                className="purple-btn1 rounded-3 px-3"
+                class="purple-btn1 rounded-3 px-3"
                 fdprocessedid="xn3e6n"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
@@ -28,15 +30,24 @@ const RuleEngine = () => {
                   width="19"
                   height="19"
                   fill="currentColor"
-                  className="bi bi-plus mb-1"
+                  class="bi bi-plus mb-1"
                   viewBox="0 0 16 16"
                 >
                   <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"></path>
                 </svg>
                 <span>New Rule</span>
               </button>
+              </Link>
             </div>
-            <div className="d-flex flex-wrap justify-content-end">
+            <div className="d-flex justify-content-end">
+            <button
+                class="purple-btn2 rounded-3 mt-2 me-3"
+                fdprocessedid="xn3e6n"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                Filter
+              </button>
               <div className="d-flex search-input w-50 p-1 ms-0 me-3">
                 <span className="material-symbols-outlined"> search </span>
                 <input
@@ -47,7 +58,7 @@ const RuleEngine = () => {
                 />
               </div>
               <button
-                className="purple-btn1 rounded-3 px-3"
+                class="purple-btn1 rounded-3 px-3"
                 fdprocessedid="xn3e6n"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
@@ -55,7 +66,7 @@ const RuleEngine = () => {
                 Go!
               </button>
               <button
-                className="purple-btn2 rounded-3 mt-2"
+                class="purple-btn2 rounded-3 mt-2"
                 fdprocessedid="xn3e6n"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
@@ -64,8 +75,8 @@ const RuleEngine = () => {
               </button>
             </div>
           </div>
-          <div className="tbl-container mx-3 mt-4">
-            <table className="w-100">
+          <div class="tbl-container mx-3 mt-4">
+            <table class="w-100">
               <thead>
                 <tr>
                   <th>Rule Name</th>
@@ -97,17 +108,19 @@ const RuleEngine = () => {
                     </span>
                   </td>
                   <td>
+                
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
                       height="16"
                       fill="currentColor"
-                      className="bi bi-eye"
+                      class="bi bi-eye"
                       viewBox="0 0 16 16"
                     >
                       <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
                       <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
                     </svg>
+                  
                   </td>
                 </tr>
                 <tr>
@@ -132,7 +145,7 @@ const RuleEngine = () => {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      className="bi bi-eye"
+                      class="bi bi-eye"
                       viewBox="0 0 16 16"
                     >
                       <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
