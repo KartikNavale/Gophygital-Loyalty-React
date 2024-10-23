be7c25fc2b30c052093428e3e2dbf8a835e26e21
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2024-10-23 14:10:20 +0530

    feat: transaction get api done

--- src/pages/member-details.jsx
+++ src/pages/member-details.jsx
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
-import Footer from '../components/Footer';
-import '../styles/style.css'
-import SubHeader from '../components/SubHeader';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import "../styles/style.css";
+import SubHeader from "../components/SubHeader";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const MemberDetails = () => {
   const { id } = useParams(); // Get the member ID from the URL
   const [member, setMember] = useState(null);
+  const [transactionData, setTransactionData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-
   const getMemberDetails = async (id) => {
     try {
-      const response = await axios.get(`https://staging.lockated.com/loyalty/members/${id}.json?token=bfa5004e7b0175622be8f7e69b37d01290b737f82e078414`);
-      console.log(response.data)
+      const response = await axios.get(
+        `https://staging.lockated.com/loyalty/members/${id}.json?token=bfa5004e7b0175622be8f7e69b37d01290b737f82e078414`
+      );
+      console.log(response.data);
       return response.data;
-
     } catch (error) {
-      console.error('Error fetching member details:', error);
+      console.error("Error fetching member details:", error);
       throw error;
     }
   };
@@ -41,7 +42,37 @@ const MemberDetails = () => {
     fetchMember();
   }, [id]);
 
+  const getTransactionData = async () => {
+    try {
+      const response = await axios.get(
+        `https://staging.lockated.com/loyalty/member_transactions.json?token=bfa5004e7b0175622be8f7e69b37d01290b737f82e078414`
+      );
+      const transactions = response.data.map(transaction => {
+        // Format the created_at date
+        const formattedDate = new Intl.DateTimeFormat('en-GB', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+        }).format(new Date(transaction.created_at));
+  
+        return {
+          ...transaction,
+          created_at: formattedDate,
+        };
+      });
+  
+      console.log("transaction data", transactions);
+      setTransactionData(transactions);
+    } catch (error) {
+      console.error("Error fetching member details:", error);
+      throw error;
+    }
+  };
+  
 
+  useEffect(() => {
+    getTransactionData()
+  }, []);
 
   return (
     <>
@@ -52,7 +83,8 @@ const MemberDetails = () => {
           <SubHeader />
           <div className="module-data-section mt-2 mb-2">
             <p className="pointer">
-              <span className="text-secondary">Members</span> &gt; Member Details
+              <span className="text-secondary">Members</span> &gt; Member
+              Details
             </p>
 
             {/* personal details */}
@@ -68,25 +100,38 @@ const MemberDetails = () => {
                   </h5>
                   <div class="row px-3">
                     <div class="col-lg-8 col-md-12 col-sm-12 row px-3">
-
-                      <div class="col-6 p-1 text-muted member-detail-color">Full name</div>
-                      <div class="col-6 p-1 member-detail-color">: {member.firstname} {member.lasttname} </div>
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Full name
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">
+                        : {member.firstname} {member.lasttname}{" "}
+                      </div>
                     </div>
                     <div class="col-lg-8 col-md-6 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Email Address</div>
-                      <div class="col-6 p-1 member-detail-color">: {member.email}</div>
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Email Address
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">
+                        : {member.email}
+                      </div>
                     </div>
                     <div class="col-lg-8 col-md-6 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Phone No.</div>
-                      <div class="col-6 p-1 member-detail-color">: {member.phone}</div>
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Phone No.
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">
+                        : {member.phone}
+                      </div>
                     </div>
                     <div class="col-lg-8 col-md-6 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Home Address</div>
-                      <div class="col-6 p-1 member-detail-color">: {member.address.address1} {member.address.address2} </div>
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Home Address
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">
+                        : {member.address.address1} {member.address.address2}{" "}
+                      </div>
                     </div>
-
                   </div>
-
                 </div>
 
                 {/* Membership Status */}
@@ -96,64 +141,125 @@ const MemberDetails = () => {
                   </h5>
                   <div class="row px-3">
                     <div class="col-lg-8 col-md-12 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Current Loyalty Points</div>
-                      <div class="col-6 p-1 member-detail-color">: {member.current_loyalty_points}</div>
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Current Loyalty Points
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">
+                        : {member.current_loyalty_points}
+                      </div>
                     </div>
                     <div class="col-lg-8 col-md-6 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Tier Progress</div>
-                      <div class="col-6 p-1 member-detail-color">: {member.member_status.tier_progression}</div>
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Tier Progress
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">
+                        : {member.member_status.tier_progression}
+                      </div>
                     </div>
                     <div class="col-lg-8 col-md-6 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Membership Duration </div>
-                      <div class="col-6 p-1 member-detail-color">: Name</div>   {/* this attribute is not there in  json*/}
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Membership Duration{" "}
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">: Name</div>{" "}
+                      {/* this attribute is not there in  json*/}
                     </div>
                     <div class="col-lg-8 col-md-6 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Account Status</div>
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Account Status
+                      </div>
                       <div class="col-6 p-1 member-detail-color">: Active</div>
                     </div>
                     <div class="col-lg-8 col-md-6 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Enrolled Date</div>
-                      <div class="col-6 p-1 member-detail-color">: {member.created_at}</div>
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Enrolled Date
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">
+                        : {member.created_at}
+                      </div>
                     </div>
                     <div class="col-lg-8 col-md-6 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Tier Level</div>
-                      <div class="col-6 p-1 member-detail-color">: {member.member_status.tier_level}</div>
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Tier Level
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">
+                        : {member.member_status.tier_level}
+                      </div>
                     </div>
                     <div class="col-lg-8 col-md-6 col-sm-12 row px-3">
-                      <div class="col-6 p-1 text-muted member-detail-color">Expiry Points</div>
-                      <div class="col-6 p-1 member-detail-color">: 20/02/2024 </div>   {/* this attribute is not there in  json*/}
+                      <div class="col-6 p-1 text-muted member-detail-color">
+                        Expiry Points
+                      </div>
+                      <div class="col-6 p-1 member-detail-color">
+                        : 20/02/2024{" "}
+                      </div>{" "}
+                      {/* this attribute is not there in  json*/}
                     </div>
-
                   </div>
                 </div>
-
-
-
 
                 {/* Middle Boxex */}
 
                 <div className="material-boxes m-5">
-                  <div className="container-fluid d-flex align-item-center justify-content-center " style={{ height: '135px', width: '1000px' }}>
+                  <div
+                    className="container-fluid d-flex align-item-center justify-content-center "
+                    style={{ height: "135px", width: "1000px" }}
+                  >
                     <div className="row d-flex justify-content-between align-item-center">
                       <div className="col-md-2 col-sm-11 d-flex justify-content-center align-item-center">
-                        <div className="content-box text-center tab-button border pt-4" style={{ height: '135px', width: '246px', borderRadius: '20px' }}>
+                        <div
+                          className="content-box text-center tab-button border pt-4"
+                          style={{
+                            height: "135px",
+                            width: "246px",
+                            borderRadius: "20px",
+                          }}
+                        >
                           <p className="content-box-sub fw-light">14.28%</p>
-                          <h6 className="content-box-title" style={{ heigth: '20px', width: '221px' }}>ALL THE POINTS EARNED</h6>
+                          <h6
+                            className="content-box-title"
+                            style={{ heigth: "20px", width: "221px" }}
+                          >
+                            ALL THE POINTS EARNED
+                          </h6>
                           <h6 className="content-box-title">1400</h6>
                         </div>
                       </div>
 
                       <div className="col-md-2 col-sm-11 d-flex justify-content-center align-item-center">
-                        <div className="content-box text-center tab-button border pt-4" style={{ height: '135px', width: '246px', borderRadius: '20px' }}>
+                        <div
+                          className="content-box text-center tab-button border pt-4"
+                          style={{
+                            height: "135px",
+                            width: "246px",
+                            borderRadius: "20px",
+                          }}
+                        >
                           <p className="content-box-sub fw-light">12.50%</p>
-                          <h6 className="content-box-title" style={{ heigth: '20px', width: '221px' }}>ALL THE POINTS REDEEMED</h6>
+                          <h6
+                            className="content-box-title"
+                            style={{ heigth: "20px", width: "221px" }}
+                          >
+                            ALL THE POINTS REDEEMED
+                          </h6>
                           <h6 className="content-box-title">7000</h6>
                         </div>
                       </div>
 
                       <div className="col-md-2 col-sm-11 d-flex justify-content-center align-item-center">
-                        <div className="content-box text-center tab-button border pt-5" style={{ height: '135px', width: '246px', borderRadius: '20px' }}>
-                          <h6 className="content-box-title" style={{ heigth: '20px', width: '221px' }}>BALANCED POINTS</h6>
+                        <div
+                          className="content-box text-center tab-button border pt-5"
+                          style={{
+                            height: "135px",
+                            width: "246px",
+                            borderRadius: "20px",
+                          }}
+                        >
+                          <h6
+                            className="content-box-title"
+                            style={{ heigth: "20px", width: "221px" }}
+                          >
+                            BALANCED POINTS
+                          </h6>
                           <h6 className="content-box-title">7000</h6>
                         </div>
                       </div>
@@ -161,16 +267,13 @@ const MemberDetails = () => {
                   </div>
                 </div>
 
-
-
-
                 {/* table */}
 
                 <div>
                   <h5 className="m-3 title ps-2 ">TRANSACTION STATUS</h5>
-                  <div className='tbl-container mx-5' >
-                    <table className="w-100"  >
-                      <thead >
+                  <div className="tbl-container mx-5">
+                    <table className="w-100">
+                      <thead>
                         <tr>
                           <td className="text-center"> Date</td>
                           <td className="text-center"> Transaction Type</td>
@@ -180,48 +283,27 @@ const MemberDetails = () => {
                         </tr>
                       </thead>
                       <tbody>
+                        {transactionData.map((item, id) => (
                         <tr>
-                          <td className="text-center">12/02/2024</td>
-                          <td className="text-center">Credit</td>
+                          <td className="text-center">{item.created_at}</td>
+                          <td className="text-center">{item.transaction_type}</td>
                           <td className="text-center">...</td>
                           <td className="text-center">...</td>
-                          <td className="text-center"></td>
+                          <td className="text-center">...</td>
                         </tr>
-                        <tr>
-                          <td className="text-center">12/02/2024</td>
-                          <td className="text-center">Debit</td>
-                          <td className="text-center">...</td>
-                          <td className="text-center">...</td>
-                          <td className="text-center"></td>
-                        </tr>
-                        <tr>
-                          <td className="text-center">12/02/2024</td>
-                          <td className="text-center">Debit</td>
-                          <td className="text-center">...</td>
-                          <td className="text-center">...</td>
-                          <td className="text-center"></td>
-                        </tr>
-                        <tr>
-                          <td className="text-center">12/02/2024</td>
-                          <td className="text-center">Credit</td>
-                          <td className="text-center">...</td>
-                          <td className="text-center">...</td>
-                          <td className="text-center"></td>
-                        </tr>
+                        ))}
                       </tbody>
                     </table>
                   </div>
-
                 </div>
-
-              </>)}
-
+              </>
+            )}
           </div>
         </div>
         <Footer />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MemberDetails
+export default MemberDetails;
