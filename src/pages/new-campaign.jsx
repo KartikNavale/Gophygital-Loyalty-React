82bcd91e53cc6fc02c9c2ee6f2f0a9f6c85543b3
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2024-10-25 10:59:04 +0530

    new campaign side bar space removed

--- src/pages/new-campaign.jsx
+++ src/pages/new-campaign.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
-import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
-import Footer from '../components/Footer';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import { useNavigate } from "react-router-dom";
 
 const NewCampaign = () => {
   const navigate = useNavigate();
 
   // Form validation schema
   const validationSchema = Yup.object({
-    name: Yup.string().required('Campaign name is required.'),
-    target_audiance: Yup.string().required('Target audience is required.'),
-    campaign_type: Yup.string().required('Campaign type is required.'),
-    loyalty_tier_id: Yup.string().required('Tier level is required.'),
+    name: Yup.string().required("Campaign name is required."),
+    target_audiance: Yup.string().required("Target audience is required."),
+    campaign_type: Yup.string().required("Campaign type is required."),
+    loyalty_tier_id: Yup.string().required("Tier level is required."),
   });
 
   const formik = useFormik({
     initialValues: {
-      name: '',
-      target_audiance: '',
-      campaign_type: '',
-      loyalty_tier_id: '',
+      name: "",
+      target_audiance: "",
+      campaign_type: "",
+      loyalty_tier_id: "",
       campaign_reward: false,
     },
     validationSchema: validationSchema,
@@ -38,7 +38,7 @@ const NewCampaign = () => {
         },
       };
 
-      // console.log("Data to be sent:", data); 
+      // console.log("Data to be sent:", data);
 
       try {
         const response = await axios.post(
@@ -49,10 +49,13 @@ const NewCampaign = () => {
         if (response.status === 201) {
           alert("Campaign created successfully!");
           formik.resetForm();
-          navigate('/campaign');
+          navigate("/campaign");
         }
       } catch (error) {
-        console.error("Error posting data:", error.response ? error.response.data : error.message);
+        console.error(
+          "Error posting data:",
+          error.response ? error.response.data : error.message
+        );
         alert("Failed to create campaign. Please try again.");
       }
     },
@@ -60,120 +63,140 @@ const NewCampaign = () => {
 
   return (
     <>
-      <div className="website-content">
-        <div className="module-data-section mt-2">
-          <p className="pointer">
-            <span className="text-secondary">Campaign</span> &gt; New Campaign
-          </p>
-          <h5 className="mb-3 title">New Campaign</h5>
-          <form onSubmit={formik.handleSubmit} className='go-shadow me-3 pt-3'>
-            <div className="border-bottom pb-2">
-              <div className="row">
-                <div className="col-md-11">
-                  <input
-                    className="border w-100 p-2 py-2"
-                    placeholder="Enter Campaign Name"
-                    name="name"
-                    value={formik.values.name}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                  {formik.touched.name && formik.errors.name ? (
-                    <p className="text-danger">{formik.errors.name}</p>
-                  ) : null}
-                </div>
-              </div>
-              <div className="row ms-1 mt-4">
-                <fieldset className="border col-lg-3 col-md-5 col-sm-11 me-2">
-                  <legend className="float-none">Target Audience<span>*</span></legend>
-                  <select
-                    name="target_audiance"
-                    value={formik.values.target_audiance} 
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    required=""
-                  >
-                    <option value="">Select Target Audience</option>
-                    <option value="Recently Joined">Recently Joined</option>
-                    <option value="Suspended">Suspended</option>
-                    <option value="1 - purchase">1 - purchase</option>
-                    <option value="No purchase">No purchase</option>
-                  </select>
-                  {formik.touched.target_audiance && formik.errors.target_audiance ? (
-                    <p className="text-danger">{formik.errors.target_audiance}</p>
-                  ) : null}
-                </fieldset>
-
-                <fieldset className="border col-lg-3 col-md-5 col-sm-11 me-2">
-                  <legend className="float-none">Campaign Type<span>*</span></legend>
-                  <select
-                    name="campaign_type"
-                    value={formik.values.campaign_type}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    required=""
-                  >
-                    <option value="">Select Campaign Type</option>
-                    <option value="Point based">Point based</option>
-                    <option value="Discount based">Discount based</option>
-                    <option value="Referral Campaign">Referral Campaign</option>
-                    <option value="Tier - Up Campaign">Tier - Up Campaign</option>
-                    <option value="Custom Campaign">Custom Campaign</option>
-                  </select>
-                  {formik.touched.campaign_type && formik.errors.campaign_type ? (
-                    <p className="text-danger">{formik.errors.campaign_type}</p>
-                  ) : null}
-                </fieldset>
-
-                <fieldset className="border col-lg-3 col-md-5 col-sm-11">
-                  <legend className="float-none">Tier Level<span>*</span></legend>
-                  <select
-                    name="loyalty_tier_id"
-                    value={formik.values.loyalty_tier_id}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    required=""
-                  >
-                    <option value="">Select Tier Level</option>
-                    <option value="1">Bronze</option>
-                    <option value="2">Silver</option>
-                    <option value="3">Gold</option>
-                  </select>
-                  {formik.touched.loyalty_tier_id && formik.errors.loyalty_tier_id ? (
-                    <p className="text-danger">{formik.errors.loyalty_tier_id}</p>
-                  ) : null}
-                </fieldset>
-              </div>
-            </div>
-            <div className='mt-2'>
-              <p className='fw-bold'>Points Criteria <span>*</span></p>
-              <p>
+      <div className="module-data-section mt-2">
+        <p className="pointer">
+          <span className="text-secondary">Campaign</span> &gt; New Campaign
+        </p>
+        <h5 className="mb-3 title">New Campaign</h5>
+        <form onSubmit={formik.handleSubmit} className="go-shadow me-3 pt-3">
+          <div className="border-bottom pb-2">
+            <div className="row">
+              <div className="col-md-11">
                 <input
-                  className="align-middle mx-2"
-                  type="checkbox"
-                  name="campaign_reward"
-                  checked={formik.values.campaign_reward}
+                  className="border w-100 p-2 py-2"
+                  placeholder="Enter Campaign Name"
+                  name="name"
+                  value={formik.values.name}
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
-                <span className="align-middle">Send points to existing members.</span>
-              </p>
-            </div>
-            <div className='mt-5'>
-              <p className='fw-bold'>Campaign Rewards <span>*</span></p>
-            </div>
-            <div className="row mt-2 justify-content-center">
-              <div className="col-md-2">
-                <button type="submit" className="purple-btn1 w-100">Create Campaign</button>
-              </div>
-              <div className="col-md-2">
-                <button type="button" className="purple-btn2 w-100" onClick={formik.handleReset}>Cancel</button>
+                {formik.touched.name && formik.errors.name ? (
+                  <p className="text-danger">{formik.errors.name}</p>
+                ) : null}
               </div>
             </div>
-          </form>
-        </div>
+            <div className="row ms-1 mt-4">
+              <fieldset className="border col-lg-3 col-md-5 col-sm-11 me-2">
+                <legend className="float-none">
+                  Target Audience<span>*</span>
+                </legend>
+                <select
+                  name="target_audiance"
+                  value={formik.values.target_audiance}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  required=""
+                >
+                  <option value="">Select Target Audience</option>
+                  <option value="Recently Joined">Recently Joined</option>
+                  <option value="Suspended">Suspended</option>
+                  <option value="1 - purchase">1 - purchase</option>
+                  <option value="No purchase">No purchase</option>
+                </select>
+                {formik.touched.target_audiance &&
+                formik.errors.target_audiance ? (
+                  <p className="text-danger">{formik.errors.target_audiance}</p>
+                ) : null}
+              </fieldset>
+
+              <fieldset className="border col-lg-3 col-md-5 col-sm-11 me-2">
+                <legend className="float-none">
+                  Campaign Type<span>*</span>
+                </legend>
+                <select
+                  name="campaign_type"
+                  value={formik.values.campaign_type}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  required=""
+                >
+                  <option value="">Select Campaign Type</option>
+                  <option value="Point based">Point based</option>
+                  <option value="Discount based">Discount based</option>
+                  <option value="Referral Campaign">Referral Campaign</option>
+                  <option value="Tier - Up Campaign">Tier - Up Campaign</option>
+                  <option value="Custom Campaign">Custom Campaign</option>
+                </select>
+                {formik.touched.campaign_type && formik.errors.campaign_type ? (
+                  <p className="text-danger">{formik.errors.campaign_type}</p>
+                ) : null}
+              </fieldset>
+
+              <fieldset className="border col-lg-3 col-md-5 col-sm-11">
+                <legend className="float-none">
+                  Tier Level<span>*</span>
+                </legend>
+                <select
+                  name="loyalty_tier_id"
+                  value={formik.values.loyalty_tier_id}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  required=""
+                >
+                  <option value="">Select Tier Level</option>
+                  <option value="1">Bronze</option>
+                  <option value="2">Silver</option>
+                  <option value="3">Gold</option>
+                </select>
+                {formik.touched.loyalty_tier_id &&
+                formik.errors.loyalty_tier_id ? (
+                  <p className="text-danger">{formik.errors.loyalty_tier_id}</p>
+                ) : null}
+              </fieldset>
+            </div>
+          </div>
+          <div className="mt-2">
+            <p className="fw-bold">
+              Points Criteria <span>*</span>
+            </p>
+            <p>
+              <input
+                className="align-middle mx-2"
+                type="checkbox"
+                name="campaign_reward"
+                checked={formik.values.campaign_reward}
+                onChange={formik.handleChange}
+              />
+              <span className="align-middle">
+                Send points to existing members.
+              </span>
+            </p>
+          </div>
+          <div className="mt-5">
+            <p className="fw-bold">
+              Campaign Rewards <span>*</span>
+            </p>
+          </div>
+          <div className="row mt-2 justify-content-center">
+            <div className="col-md-2">
+              <button type="submit" className="purple-btn1 w-100">
+                Create Campaign
+              </button>
+            </div>
+            <div className="col-md-2">
+              <button
+                type="button"
+                className="purple-btn2 w-100"
+                onClick={formik.handleReset}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </>
   );
-}
+};
 
 export default NewCampaign;
