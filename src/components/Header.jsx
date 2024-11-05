410d51dec9694f6184c61542514fe0f7470323ac
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2024-11-05 13:28:38 +0530

    fix: fixed sidebar active state and header for tier

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../styles/style.css";
 import GophygitalLogo1 from "/GophygitalLogo1.svg";
 import { useNavigate } from "react-router-dom";
-import Form from "react-bootstrap/Form";
-import { Button } from "react-bootstrap";
 import TypeHeader from "./TypeHeader";
 
-const Header = () => {
+const Header = ({ noTier }) => {
   const [showModal, setShowModal] = useState(false);
-
-  
   const navigate = useNavigate();
-
-
-
-
-
-
 
   const handleClose = () => {
     setShowModal(false);
@@ -26,17 +16,15 @@ const Header = () => {
     setShowModal(true);
   };
 
-
   const signout = () => {
     console.log("Signing out...");
-
     sessionStorage.clear(); // Example: clear session storage on sign out
     navigate("/login");
   };
 
   return (
     <>
-          <div
+      <div
         className="modal"
         id="userInfo"
         aria-labelledby="userInfoLabel"
@@ -50,7 +38,6 @@ const Header = () => {
               <button
                 type="button"
                 className="btn-close"
-                data-bs-dismiss="modal"
                 aria-label="Close"
                 onClick={handleClose}
               />
@@ -61,13 +48,9 @@ const Header = () => {
               </div>
               <br />
               <h5>{sessionStorage.getItem("firstname") || "First Name"}</h5>
-              {/* Fallback for dynamic Name */}
               <p>{sessionStorage.getItem("email") || "example@example.com"}</p>
-              {/* Fallback for dynamic Email */}
-            
               <button
                 className="purple-btn1 my-3"
-                data-bs-dismiss="modal"
                 aria-label="Close"
                 onClick={signout}
               >
@@ -77,36 +60,24 @@ const Header = () => {
           </div>
         </div>
       </div>
+
       <nav className="navbar navbar-expand-lg navbar-light p-0">
         <div className="container-fluid py-1">
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            onclick="openNav()"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <img alt="logo" className="go-logo mx-3 my-2" src={GophygitalLogo1} />
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo02"
             aria-controls="navbarTogglerDemo02"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon" />
           </button>
+          <img alt="logo" className="go-logo mx-3 my-2" src={GophygitalLogo1} />
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a
-                  className="nav-link"
-                  aria-current="page"
-                  href="./1CF_Main.html"
-                >
+                <a className="nav-link" href="./1CF_Main.html">
                   Home
                 </a>
               </li>
@@ -116,44 +87,39 @@ const Header = () => {
                 </a>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link active rounded-2"
-                  href="./31Dashboard_Daily.html"
-                >
+                <a className="nav-link active rounded-2" href="./31Dashboard_Daily.html">
                   Setup
                 </a>
               </li>
             </ul>
-            <div className="top-nav-right">
-              <div className="d-flex search-input w-50 mx-auto">
-                <span className="material-symbols-outlined"> search </span>
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
+            {!noTier && (
+              <div className="top-nav-right">
+                <div className="d-flex search-input w-50 mx-auto">
+                  <span className="material-symbols-outlined">search</span>
+                  <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                </div>
               </div>
-            </div>
+            )}
           </div>
-          
 
           <TypeHeader /> {/* Dropdown inside the header */}
 
-    <div
-        className="avatar"
-        role="button"
-        tabIndex="0"
-        onClick={handleOpen} // Open the modal on click
-        onKeyPress={(e) => e.key === 'Enter' && handleOpen()} // Open on Enter key press
-        data-bs-toggle="modal" // Optional if you want Bootstrap's behavior too
-        data-bs-target="#userInfo"
-      >
-        <div className="avatar__letters">A</div>
-      </div>
-
-
-
+          <div
+            className="avatar"
+            role="button"
+            tabIndex="0"
+            onClick={handleOpen} // Open the modal on click
+            onKeyPress={(e) => e.key === 'Enter' && handleOpen()} // Open on Enter key press
+            data-bs-toggle="modal" // Optional if you want Bootstrap's behavior too
+            data-bs-target="#userInfo"
+          >
+            <div className="avatar__letters">A</div>
+          </div>
         </div>
       </nav>
     </>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import "../styles/style.css";
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const [activeItem, setActiveItem] = useState("/"); // Set default active item
+
+  const location = useLocation(); // Get the current location
+
+  // Only set the active item if it is the first load
+  useEffect(() => {
+    // Only set the active item if it's the initial render
+    if (activeItem === "/") {
+      setActiveItem(location.pathname);
+    }
+  }, [location.pathname]);
 
   useEffect(() => {
     const collapseElement = document.getElementById("userRoleCollapse");
@@ -28,193 +38,188 @@ const Sidebar = () => {
     };
   }, []);
 
-  return (
-    <>
-      <div className="sidebar sidebar_inner" id="mySidebar">
-        <p className="closebtn">
-          <span
-            className="material-symbols-outlined cancel"
-            onclick="closeNav()"
-          >
-            cancel
-          </span>
-        </p>
-        <ul
-          className="px-2 collapse left-1"
-          style={{ display: "block" }}
-          id="sidebarCollapse"
-        >
-          {/* button for closing sidebar*/}
-          <li className="nav-item">
-            <Link
-              to=""
-              className="nav-link d-flex justify-content-between"
-              aria-current="page"
-            >
-              {/* <img alt="" src="" /> */}
-              <span className="text">Dashboard</span>
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-caret-right-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                </svg>
-              </span>
-            </Link>
-          </li>
-          {/* <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
-              <img alt="" src="" />
-              <span className="text">Members</span>
-            </a>
-          </li> */}
-          <li className="nav-item">
-            <Link
-              to="/members"
-              className="nav-link d-flex justify-content-between"
-              aria-current="page"
-            >
-              {/* <img alt="" src="" /> */}
-              <span className="text">Members</span>
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-caret-right-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                </svg>
-              </span>
-            </Link>
-          </li>
-          {/* <li className="nav-item">
-            <a className="nav-link" aria-current="page" href="#">
-              <img alt="" src="" />
-              <span className="text">Tiers</span>
-            </a>
-          </li> */}
-          <li className="nav-item">
-            <Link
-              to="/tiers"
-              className="nav-link d-flex justify-content-between"
-              aria-current="page"
-            >
-              {/* <img alt="" src="" /> */}
-              <span className="text">Tiers</span>
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-caret-right-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                </svg>
-              </span>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/rule-engine"
-              className="nav-link d-flex justify-content-between"
-              aria-current="page"
-            >
-              {/* <img alt="" src="" /> */}
-              <span className="text">Rule Engine</span>
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-caret-right-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                </svg>
-              </span>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <a
-              className="text nav-link d-flex justify-content-between"
-              data-bs-toggle="collapse"
-              data-bs-target="#userRoleCollapse"
-            >
-              <span className="text">Engage</span>
-              {isCollapsed ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-caret-down-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-caret-right-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                </svg>
-              )}
-            </a>
-            <ul className="collapse p-0" id="userRoleCollapse">
-              <li>
-                <Link className="ps-4" to="/Segment">
-                  Segment
-                </Link>
-              </li>
-              <li>
-                <Link className="ps-4" to="/Campaign">
-                  Campaign
-                </Link>
-              </li>
-            </ul>
-          </li>
+  // Function to close the sidebar
+  const closeNav = () => {
+    const sidebar = document.getElementById("mySidebar");
+    if (sidebar) {
+      sidebar.style.display = "none"; // Change to your desired closing behavior
+    }
+  };
 
-          <li className="nav-item">
-            <Link
-              to="/test"
-              className="nav-link d-flex justify-content-between"
-              aria-current="page"
-            >
-              {/* <img alt="" src="" /> */}
-              <span className="text">Reports</span>
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-caret-right-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                </svg>
-              </span>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </>
+  const handleNavLinkClick = (path) => {
+    // Only update the active item if the clicked path is different from the current active item
+    if (activeItem !== path) {
+      setActiveItem(path);
+    }
+  };
+
+  return (
+    <div className="sidebar sidebar_inner" id="mySidebar">
+      <p className="closebtn">
+        <span className="material-symbols-outlined cancel" onClick={closeNav}>
+          cancel
+        </span>
+      </p>
+      <ul className="px-2 collapse left-1" style={{ display: "block" }} id="sidebarCollapse">
+        <li className="nav-item">
+          <NavLink
+            to="/"
+            className={`nav-link d-flex justify-content-between ${activeItem === "/" ? "active" : ""}`}
+            onClick={() => handleNavLinkClick("/")}
+          >
+            <span className="text">Dashboard</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-caret-right-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+              </svg>
+            </span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="/members"
+            className={`nav-link d-flex justify-content-between ${activeItem === "/members" ? "active" : ""}`}
+            onClick={() => handleNavLinkClick("/members")}
+          >
+            <span className="text">Members</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-caret-right-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+              </svg>
+            </span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="/tiers"
+            className={`nav-link d-flex justify-content-between ${activeItem === "/tiers" ? "active" : ""}`}
+            onClick={() => handleNavLinkClick("/tiers")}
+          >
+            <span className="text">Tiers</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-caret-right-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+              </svg>
+            </span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="/rule-engine"
+            className={`nav-link d-flex justify-content-between ${activeItem === "/rule-engine" ? "active" : ""}`}
+            onClick={() => handleNavLinkClick("/rule-engine")}
+          >
+            <span className="text">Rule Engine</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-caret-right-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+              </svg>
+            </span>
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <a
+            className="text nav-link d-flex justify-content-between"
+            data-bs-toggle="collapse"
+            data-bs-target="#userRoleCollapse"
+          >
+            <span className="text">Engage</span>
+            {isCollapsed ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-caret-down-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-caret-right-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+              </svg>
+            )}
+          </a>
+          <ul className="collapse p-0" id="userRoleCollapse">
+            <li>
+              <NavLink
+                className={`ps-4 ${activeItem === "/Segment" ? "active" : ""}`}
+                to="/Segment"
+                onClick={() => handleNavLinkClick("/Segment")}
+              >
+                Segment
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={`ps-4 ${activeItem === "/Campaign" ? "active" : ""}`}
+                to="/Campaign"
+                onClick={() => handleNavLinkClick("/Campaign")}
+              >
+                Campaign
+              </NavLink>
+            </li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            to="/test"
+            className={`nav-link d-flex justify-content-between ${activeItem === "/services" ? "active" : ""}`}
+            onClick={() => handleNavLinkClick("/services")}
+          >
+            <span className="text">Reports</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-caret-right-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+              </svg>
+            </span>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 };
 
